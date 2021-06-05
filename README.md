2254f2d84c00fbf7fff30e6f8c82c324f1ca18f3
karen <k.cadenasherrera@gmail.com>
2021-06-05 01:08:10 -0400

    solucion del hosting

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
 # mundo-verde
 
-## Project setup
+## Descripción del proyecto
+Es un proyecto familiar que consta en la venta de plantas vivas para la decoración del hogar y jardín en general.
+Cuyo próposito es facilitar el conocimiento, cuidado de las especies para sus mantención.Esta destinado a toda persona que deseen agregar espacios verdes y naturalidad al hogar.
+
+## Guía de estilos
+Colors Scheme
+
+white #ffffff
+green #008000
+aqua #00ffff
+teal #008080
+blue #0000ff
+
+ Typography
+
+ Typefaces
+
+ Roboto regular
+ Roboto light
+
+ Font Sizes
+ h1 50px  xx-large 
+ p  18px large
+
+ ## components
+
+ Buttons
+ Size small button 
+ states normal state
+
+
+## Comandos del proyecto
+vue CLI
+firebase CLI
+vue create
+firebase init
+vuetify
+
+
+
+
+### Project setup
 ```
 npm install
 ```
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home'
 import Vuex from 'vuex'
 
+
 Vue.use(VueRouter)
 Vue.use(Vuex)
 
@@ -13,13 +14,19 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/',
+    name: 'Favoritos',
+
+
+  },
+  //{
+  //path: '/',
+  //name: 'About',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  //}
 ]
 
 const router = new VueRouter({
