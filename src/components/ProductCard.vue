1ed3cc6dcc5a156ce48b7a664d9c1cd37bf78ce1
karen <k.cadenasherrera@gmail.com>
2021-06-12 23:47:46 -0400

    Agregar campo nombre a los productos

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -2,7 +2,8 @@
   <v-card class="ma-5" max-width="400">
     <v-img class="white--text align-end" height="200px" :src="producto.image" />
 
-    <v-card-title>{{ producto.price | toCurrency }}</v-card-title>
+    <v-card-title>{{ producto.name }}</v-card-title>
+    <v-card-subtitle>{{ producto.price | toCurrency }}</v-card-subtitle>
 
     <v-card-actions>
       <v-btn
